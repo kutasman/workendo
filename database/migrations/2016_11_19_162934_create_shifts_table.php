6b38bc1de230157fee6b2cb382971693fc59bf63
Eugene Kutas <evgenij.kutas@gmail.com>
2016-11-19 19:25:07 +0200

    Add shifts model and controller

---
+++ database/migrations/2016_11_19_162934_create_shifts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->increments('id');
+	        $table->date('date');
+	        $table->smallInteger('songs', false, true);
+	        $table->integer('place_id', false, true);
+	        $table->integer('user_id', false, true);
+	        $table->timestamps();
+
+	        $table->foreign('place_id')->references('id')->on('places');
+	        $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    Schema::disableForeignKeyConstraints();
+
+	    Schema::dropIfExists('shifts');
+
+	    Schema::enableForeignKeyConstraints();
+    }
+}
